4fc15eda6a1d9ec39241fb0c2d8c4f7b43890bf0
zhao wen qiang <13143410521@163.com>
2018-12-04 02:44:31 +0800

    fix

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+          alias: {
+            '@images': './images'
+          }
+        }
+    },
+    title: 'aries-logger-services',
+    description: 'log for aries-services',
+    head: [
+        ['link', { rel: 'icon', href: 'http://www.semantic-ui.cn/images/logo.png' }]
+    ],
+    themeConfig: {
+        nav: [{
+                text: 'siwi',
+                link: 'https://siwi.me'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/siwilizhao'
+            }
+        ]
+    }
+}
